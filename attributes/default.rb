b46bebdf90224b80de8117921cca1188dda4f9c6
svanzee4cb <svanzee@Van-Zees-MacBook-Pro.local>
2015-01-16 13:04:46 -0600

    change project path to /var/www

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,5 +8,5 @@ default['cbplatform']['server']['aliases'] = ['caringbridge.dev',
             'vagrant.cbtiny.org'
 ],
 default['cbplatform']['server']['ip_address'] = '33.33.33.41'
-default['cbplatform']['project_path'] = '/opt/platform' # FIXME: should be /var/www/platform
+default['cbplatform']['project_path'] = '/var/www/platform' # FIXME: should be /var/www/platform
 # default['cbplatform']['admin_email'] = ENV['LOCAL_USER'] + '@caringbridge.org' FIXME: It would be nice to have access to an individual email address somehow
